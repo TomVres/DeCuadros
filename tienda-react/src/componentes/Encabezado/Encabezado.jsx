14dc5212351c707f9d87816e3255206a80b464d6
TomVres <tomvresps@gmail.com>
2021-06-14 20:56:44 -0300

    Commit componentes

---
+++ tienda-react/src/componentes/Encabezado/Encabezado.jsx
@@ -0,0 +1,35 @@
+import "./style.css"
+export const Encabezado = () => {
+
+return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+        <a className="navbar-brand" href="#">Mi Tienda React</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto mr-5">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Inicio <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Productos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Nosotros</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacto</a>
+            </li>
+          </ul>
+          <form className="form-inline">
+            <input className="form-control mr-sm-2" type="search" placeholder="Buscar un producto" aria-label="Buscar" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+          </form>
+        </div>
+        </div>
+      </nav>
+)
+
+}
